2aad935bdec08e3b52a4fc061193da44ec34ba2d
cardenasnavarro <fredycn7sa@gmail.com>
2015-11-13 06:02:43 -0500

    calcular

--- simulador/simuElectric/www/js/index_user_scripts.js
+++ simulador/simuElectric/www/js/index_user_scripts.js
@@ -5,36 +5,38 @@
 
     function register_event_handlers()
     {
-        $(document).on("click", "#monocalcular", function (evt)
-            {
+    
+                      
+        $(document).on("click", "#monocalcular", function(evt)
+        {
 
-             var lecActual = document.getElementById('LecturaActual');
-             var lecAnterior = document.getElementById('LecturaAnterior');
-             var igv = 0.18;
-             var consumo = 0;
-             var mantenimiento = 0.67;
-             var costoKWh = 0.495;
-             var electrificacion = 0.42;
-             var  alum_public= 0.3623;
+         var lecActual = document.getElementById('LecturaActual');
+         var lecAnterior = document.getElementById('LecturaAnterior');
+         var igv = 0.18;
+         var consumo = 0;
+         var mantenimiento = 0.67;
+         var costoKWh = 0.495;
+         var electrificacion = 0.42;
+         var  alum_public= 0.3623;
+ 
+         var cargofijo = 3.59;
+         var intereses = 0.08;
 
-             var cargofijo = 3.59;
-             var intereses = 0.08;
+        
+           if((lecAnterior.value)*1 < (lecActual.value)*1)
+           {  
+         
+             var diferenciaLectura = (lecActual.value)*1 - (lecAnterior.value)*1;
+            
 
-
-               if((lecAnterior.value)*1 < (lecActual.value)*1)
-               {  
-             
-                 var diferenciaLectura = (lecActual.value)*1 - (lecAnterior.value)*1;
-                
-
-                   if (diferenciaLectura<=30) {
+               if (diferenciaLectura<=30) {
                      
                      alert("USTED TIENE LA POSIBILIDAD DE AFILIARSE AL FISE");
 
                      var energia = diferenciaLectura* costoKWh;
                      var costoAlumbrado= 1 * alum_public;
                     
-                    var subtotal = energia + costoAlumbrado + cargofijo + mantenimiento;
+                     var subtotal = energia + costoAlumbrado + cargofijo + mantenimiento;
                        
                      var sbtt1= subtotal.toFixed(3);
                      var total = subtotal + subtotal * igv;
@@ -197,8 +199,9 @@
                      $('#idimprimir')[0].innerHTML='USTED CONSUMIO :'+'<div style="color:#8B4513">'+diferenciaLectura+' KW </div><br>'+'EL SUBTOTAL ES: '+'<div style="color:#B8860B"> S/. '+ sbtt + ' </div><br>' + 'EL PAGO A REALIZAR POR EL CONSUMO ES: '+'<div style="color:#DAA520">S/. '+ppgg+'</div><br>'+ 'NO SE OLVIDE QUE PAGE ANTES DEL 28 DE CADA MES'+'<div style ="color:#FF4500"'+'<br>';
                      
                     }
+
                    if (diferenciaLectura>10000) {
-                                    alert('SU CONSUMO ES DEMASIADO ALTO CORRIGE LOS DATOS'  );
+                     alert('SU CONSUMO ES DEMASIADO ALTO CORRIGE LOS DATOS'  );
                      
                      var energia = diferenciaLectura * costoKWh;
                      var costoAlumbrado= 350* alum_public;
@@ -212,13 +215,10 @@
                      
                     }
 
-
-
                 
+                  if((lecAnterior.value)*1==(lecActual.value)*1)
 
-                if((lecAnterior.value)*1==(lecActual.value)*1)
-
-               {  
+                  {  
                  alert("SU CONSUMO ES IGUAL");
                  var diferenciaLectura = (lecActual.value)*1 - (lecAnterior.value)*1;         
                  var costoAlumbrado= 1 * alum_public;
@@ -228,23 +228,45 @@
                  var TotalPago = total + electrificacion;
                  var ppgg= TotalPago.toFixed(2);
                  $('#idimprimir')[0].innerHTML='USTED CONSUMIO :'+'<div style="color:#8B4513">'+diferenciaLectura+' KW </div><br>'+'EL SUBTOTAL ES: '+'<div style="color:#B8860B"> S/. '+ sbtt + ' </div><br>' + 'EL PAGO A REALIZAR POR EL CONSUMO ES: '+'<div style="color:#DAA520">S/. '+ppgg+'</div><br>'+ 'NO SE OLVIDE QUE PAGE ANTES DEL 28 DE CADA MES'+'<div style ="color:#FF4500"'+'<br>';
-                }        
+                   }  
 
 
-                if((lecAnterior.value)*1 > (lecActual.value)*1)
+            }
 
-               {  
-                    alert("DATOS INCORRECTOS");
-                    var lecActual = document.getElementById('LecturaActual');
-                    var lecAnterior = document.getElementById('LecturaAnterior');     
-                        
-                    document.getElementById('idimprimir').innerHTML='VUELVA A INGRESAR LOS DATOS ';
-                    lecActual.value='';
-                    lecAnterior.value=''; 
-                } 
-               
-                           
-            }});
+            if((lecAnterior.value)*1==(lecActual.value)*1)
+
+           {  
+             alert("SU CONSUMO ES IGUAL");
+             var diferenciaLectura = (lecActual.value)*1 - (lecAnterior.value)*1;         
+             var costoAlumbrado= 1 * alum_public;
+             var subtotal = costoAlumbrado + cargofijo + mantenimiento;
+             var sbtt= subtotal.toFixed(2);
+             var total = subtotal + subtotal * igv;
+             var TotalPago = total + electrificacion;
+             var ppgg= TotalPago.toFixed(2);
+             $('#idimprimir')[0].innerHTML='USTED CONSUMIO :'+'<div style="color:#8B4513">'+diferenciaLectura+' KW </div><br>'+'EL SUBTOTAL ES: '+'<div style="color:#B8860B"> S/. '+ sbtt + ' </div><br>' + 'EL PAGO A REALIZAR POR EL CONSUMO ES: '+'<div style="color:#DAA520">S/. '+ppgg+'</div><br>'+ 'NO SE OLVIDE QUE PAGE ANTES DEL 28 DE CADA MES'+'<div style ="color:#FF4500"'+'<br>';
+            }        
+
+
+            if((lecAnterior.value)*1 > (lecActual.value)*1)
+
+           {  
+                alert("DATOS INCORRECTOS");
+                var lecActual = document.getElementById('LecturaActual');
+                var lecAnterior = document.getElementById('LecturaAnterior');     
+                    
+                document.getElementById('idimprimir').innerHTML='VUELVA A INGRESAR LOS DATOS ';
+                lecActual.value='';
+                lecAnterior.value=''; 
+            } 
+           
+                       
+
+ 
+          //activate_page("#mainpage"); 
+        
+        });
+    
         $(document).on("click", "#monocancelar", function(evt)
         {
          alert("DATOS CANCELADOS");
@@ -262,8 +284,5 @@
 
    document.addEventListener("app.Ready", register_event_handlers, false);
 })();
- 
-
-  //activate_page("#mainpage"); 
 
 
